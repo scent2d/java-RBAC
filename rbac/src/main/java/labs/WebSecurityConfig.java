2e842a96ab44ee6fe32905687e9eafe90012706a
scent2d <kimtaesu9@gmail.com>
2022-06-26 07:02:44 +0900

    rbac

--- rbac/src/main/java/labs/WebSecurityConfig.java
+++ rbac/src/main/java/labs/WebSecurityConfig.java
@@ -2,7 +2,7 @@ package labs;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
